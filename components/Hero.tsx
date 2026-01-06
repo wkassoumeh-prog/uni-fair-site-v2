8da41b07863c7d409167bcbe298706b9626ccc6c
wkassoumeh-prog <wkassoumeh@gmail.com>
2026-01-06 14:28:38 +0100

    adding puglic folder - fixing hero image attempt - edit hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -8,7 +8,7 @@ const Hero: React.FC = () => {
       <div 
         className="absolute inset-0 z-0 bg-cover bg-center bg-no-repeat"
         style={{ 
-          backgroundImage: `url('flags banner.png')`,
+          backgroundImage: `url('/images/flags banner.png')`,
         }}
       >
         <div className="absolute inset-0 bg-blue-900/60 mix-blend-multiply"></div>
